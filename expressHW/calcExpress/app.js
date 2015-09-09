bb33438426799a3ebed10a9c3c13d3501979c46d
Mark Marsella <mmarsella114@g.rwu.edu>
2015-09-09 16:07:31 -0700

    bonus complete

--- expressHW/calcExpress/app.js
+++ expressHW/calcExpress/app.js
@@ -5,55 +5,82 @@ var express = require('express'),
   //set view engine to ejs
 app.set('view engine', 'ejs');
 
+app.get('/:operator/:var1/:var2', function (req,res){
+
+  var operator = req.params.operator;
+  var varOne = req.params.var1;
+  var varTwo = req.params.var2;
+  var result;
+
+  if(operator == "add")
+  {
+    result = parseInt(varOne) + parseInt(varTwo);
+  }
+  else if(operator == "subtract")
+  {
+    result = parseInt(varOne) - parseInt(varTwo);
+
+  }
+  else if(operator == "mult")
+  {
+    result = parseInt(varOne) * parseInt(varTwo);
+  }
+  else if(operator == "div")
+  {
+    result = parseInt(varOne) + parseInt(varTwo);
+  }
+  else
+  {
+    result = "Please specify a proper operator!!!";
+  }
+
+  res.render("index", {sum: result});
+
+});
+
+
 // app.get('/', function(req, res){
 //   // use res.render
 //   res.render('index', {name: "Elie"});
 // });
 
-app.get('/add/:var1/:var2', function (req,res){
+// app.get('/add/:var1/:var2', function (req,res){
 
-  var varOne = req.params.var1;
-  var varTwo = req.params.var2;
-  var sum = parseInt(varOne) + parseInt(varTwo);
+//   var that = req.params.add;
+//   var varOne = req.params.var1;
+//   var varTwo = req.params.var2;
+//   var sum = parseInt(varOne) + parseInt(varTwo);
 
-  res.render("index", {sum: sum});
-});
-
-
-app.get('/subtract/:var1/:var2', function (req,res){
-
-  var varOne = req.params.var1;
-  var varTwo = req.params.var2;
-  var result = parseInt(varOne) - parseInt(varTwo);
-
-  res.render("index", {sum: result});
-});
-
-app.get('/div/:var1/:var2', function (req,res){
-
-  var varOne = req.params.var1;
-  var varTwo = req.params.var2;
-  var quotient = parseInt(varOne) / parseInt(varTwo);
-
-  res.render("index", {sum: quotient});
-});
-
-app.get('/mult/:var1/:var2', function (req,res){
-
-  var varOne = req.params.var1;
-  var varTwo = req.params.var2;
-  var product = parseInt(varOne) * parseInt(varTwo);
-
-  res.render("index", {sum: product});
-});
-
- 
+//   res.render("index", {sum: sum});
+// });
 
 
+// app.get('/subtract/:var1/:var2', function (req,res){
 
+//   var varOne = req.params.var1;
+//   var varTwo = req.params.var2;
+//   var result = parseInt(varOne) - parseInt(varTwo);
 
+//   res.render("index", {sum: result});
+// });
 
+// app.get('/div/:var1/:var2', function (req,res){
 
+//   var varOne = req.params.var1;
+//   var varTwo = req.params.var2;
+//   var quotient = parseInt(varOne) / parseInt(varTwo);
+
+//   res.render("index", {sum: quotient});
+// });
+
+// app.get('/mult/:var1/:var2', function (req,res){
+
+//   var varOne = req.params.var1;
+//   var varTwo = req.params.var2;
+//   var product = parseInt(varOne) * parseInt(varTwo);
+
+//   res.render("index", {sum: product});
+// });
 
 
 // start the server
