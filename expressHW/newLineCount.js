ca9b6a08d19629b26bfb5df98cb38a255c639653
Mark Marsella <mmarsella114@g.rwu.edu>
2015-09-09 14:16:34 -0700

    readFile complete

--- expressHW/newLineCount.js
+++ expressHW/newLineCount.js
@@ -1,7 +1,24 @@
 var fs = require('fs');
 
 // pass in utf-8 to have it be readbale
-var file = fs.readFileSync(process.argv[2], "utf-8");
+// var file = fs.readFileSync(process.argv[2], "utf-8");
+
+
+fs.readFile(process.argv[2], "utf-8", function (err,data){
+  console.log(data.match(/\n/g).length);
+});
+
+var file = fs.readFileSync(process.argv[2]);
+
+
+// console.log(file.toString());
+
+
+// console.log("AFTER THE READ!!!");
+
+// fs.readFile("text.txt", "utf-8", function (err,data){
+//   console.log(data);
+// });
 
 
 
@@ -9,5 +26,5 @@ var file = fs.readFileSync(process.argv[2], "utf-8");
 
 //use regex to count line #'s
 
-console.log(file.match(/\n/g).length);
+// console.log(file.match(/\n/g).length);
 
